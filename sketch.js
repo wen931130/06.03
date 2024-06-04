f6fbdf59491877983d4982a44fac3c05b9ff3ce9
wen931130 <150638551+wen931130@users.noreply.github.com>
2024-06-04 10:25:06 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -76,7 +76,7 @@ function drawSkeleton () {
   if (partB.score > 0.1) {
     push();
     textSize(40);
-    text("412730748 陳玟慈", 10,40);
+    text("412730748 陳玟慈",  partA.x-50,partA.y-100);
     pop();
   }
   if (partA.score > 0.1) {
